601325a86271fea1af93b17d09c672426eb6dd88
Thanh <thanhz@hotmail.com>
2020-04-03 02:23:56 +0100

    Updates

---
+++ precache-manifest.1da1723aa594554389108c4cc365beaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c956d441f25f7340e36c4e6dc13f77f",
+    "url": "/react-monster-search/index.html"
+  },
+  {
+    "revision": "c51ede91fc9bea186616",
+    "url": "/react-monster-search/static/css/main.b2ced40f.chunk.css"
+  },
+  {
+    "revision": "6d235154ef6fb58dc912",
+    "url": "/react-monster-search/static/js/2.d5e27730.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-monster-search/static/js/2.d5e27730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51ede91fc9bea186616",
+    "url": "/react-monster-search/static/js/main.a2d58777.chunk.js"
+  },
+  {
+    "revision": "039115ca8b065d1760d1",
+    "url": "/react-monster-search/static/js/runtime-main.41aacaf0.js"
+  }
+]);
